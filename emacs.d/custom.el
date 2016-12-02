b6724c69b72d59739320c0430dd130a5496973ba
Kartik Shenoy <theresmorethanoneway@gmail.com>
2016-12-02 04:15:48 -0700

    Put it in a hack to get emacs --daemon running

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -5,7 +5,10 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default))))
+    ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
+ '(package-selected-packages
+   (quote
+    (helm-projectile yasnippet which-key use-package rainbow-delimiters org-wunderlist org-bullets org-ac leuven-theme htmlize helm-descbinds evil-visualstar evil-surround evil-matchit evil-exchange evil-commentary company ace-window))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
