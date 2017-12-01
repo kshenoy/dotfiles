fa9a0962fc998c80950de5370c0fec5c037f4f78
Kartik Shenoy <theresmorethanoneway@gmail.com>
2017-12-01 05:51:13 -0700

    Checkpointing before declaring emacs bankruptcy

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,6 +1,5 @@
 ;; -*- mode: emacs-lisp -*-
 
-
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
