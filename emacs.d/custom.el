fb0dda462ab68c9a9564d97b59a3b9368a043c55
Kartik Shenoy <theresmorethanoneway@gmail.com>
2018-02-17 10:49:14 -0700

    Updates

---
+++ emacs.d/custom.el
@@ -0,0 +1,22 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("25c242b3c808f38b0389879b9cba325fb1fa81a0a5e61ac7cae8da9a32e2811b" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
+ '(debug-on-error t)
+ '(org-agenda-files
+   (quote
+    ("/home/kshenoy/Notes/AMD/Aspen.org" "/home/kshenoy/Notes/AMD/AspenHighlands.org" "/home/kshenoy/Notes/AMD/Breckenridge.org" "/home/kshenoy/Notes/AMD/Formal.org" "/home/kshenoy/Notes/AMD/Weekly.org")))
+ '(package-selected-packages
+   (quote
+    (async avy dash evil goto-chg helm helm-core base16-theme rainbow-mode org-drill-table org-id aggressive-indent ob-sml projectile noflet org-ac helm-projectile yasnippet which-key use-package try swiper sml-mode rainbow-delimiters org-wunderlist org-bullets leuven-theme htmlize helm-descbinds geiser flycheck evil-visualstar evil-surround evil-numbers evil-matchit evil-leader evil-exchange evil-commentary company ace-window)))
+ '(safe-local-variable-values (quote ((org-confirm-babel-evaluate)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
