256c40415eaadef14c6408673f09f8e57692b233
Kartik Shenoy <theresmorethanoneway@gmail.com>
2018-03-13 19:23:26 -0600

    Deferred loading of packages wherever possible

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -9,9 +9,8 @@
     (evil-numbers evil-args evil-string-inflection yasnippet which-key use-package string-inflection solarized-theme rainbow-mode rainbow-delimiters pcre2el org-plus-contrib org-bullets noflet flycheck evil-visualstar evil-surround evil-matchit evil-exchange evil-commentary diminish counsel company beacon aggressive-indent)))
  '(safe-local-variable-values
    (quote
-    ((org-todo-keywords
-      (quote
-       ((sequence "TODO" "DONE"))))
+    ((org-todo-keywords quote
+                        ((sequence "TODO" "DONE")))
      (org-confirm-babel-evaluate)
      (org-log-into-drawer)))))
 (custom-set-faces
