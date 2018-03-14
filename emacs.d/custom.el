483d9939230f898394c296719c97272063e711b3
Kartik Shenoy <theresmorethanoneway@gmail.com>
2018-03-14 01:02:00 -0600

    Diminish everything! Added auto-completion packages

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -6,13 +6,10 @@
  '(debug-on-error t)
  '(package-selected-packages
    (quote
-    (evil-numbers evil-args evil-string-inflection yasnippet which-key use-package string-inflection solarized-theme rainbow-mode rainbow-delimiters pcre2el org-plus-contrib org-bullets noflet flycheck evil-visualstar evil-surround evil-matchit evil-exchange evil-commentary diminish counsel company beacon aggressive-indent)))
+    (popup-kill-ring evil-numbers evil-args evil-string-inflection yasnippet which-key use-package string-inflection solarized-theme rainbow-mode rainbow-delimiters pcre2el org-plus-contrib org-bullets noflet flycheck evil-visualstar evil-surround evil-matchit evil-exchange evil-commentary diminish counsel company beacon aggressive-indent)))
  '(safe-local-variable-values
    (quote
-    ((org-todo-keywords quote
-                        ((sequence "TODO" "DONE")))
-     (org-confirm-babel-evaluate)
-     (org-log-into-drawer)))))
+    ((org-confirm-babel-evaluate)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
