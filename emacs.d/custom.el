4ad0461af09acdbc446fea8ca075a9aba8213362
Kartik Shenoy <theresmorethanoneway@gmail.com>
2018-03-15 20:02:45 -0600

    Replaced diminish with delight. Enabled pretty symbols in all programming modes

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -6,10 +6,8 @@
  '(debug-on-error t)
  '(package-selected-packages
    (quote
-    (popup-kill-ring evil-numbers evil-args evil-string-inflection yasnippet which-key use-package string-inflection solarized-theme rainbow-mode rainbow-delimiters pcre2el org-plus-contrib org-bullets noflet flycheck evil-visualstar evil-surround evil-matchit evil-exchange evil-commentary diminish counsel company beacon aggressive-indent)))
- '(safe-local-variable-values
-   (quote
-    ((org-confirm-babel-evaluate)))))
+    (delight popup-kill-ring evil-numbers evil-args evil-string-inflection yasnippet which-key use-package string-inflection solarized-theme rainbow-mode rainbow-delimiters pcre2el org-plus-contrib org-bullets noflet flycheck evil-visualstar evil-surround evil-matchit evil-exchange evil-commentary diminish counsel company beacon aggressive-indent)))
+ '(safe-local-variable-values (quote ((org-confirm-babel-evaluate)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
