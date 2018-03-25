623688590b9a2a0ed5bec0e123ef355fc70afda9
Kartik Shenoy <theresmorethanoneway@gmail.com>
2018-03-25 12:55:10 -0600

    Added avy, modern-c++-font-lock and nlinum-relative

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -4,9 +4,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(debug-on-error t)
+ '(nlinum-relative-redisplay-delay 0)
  '(package-selected-packages
    (quote
-    (delight popup-kill-ring evil-numbers evil-args evil-string-inflection yasnippet which-key use-package string-inflection solarized-theme rainbow-mode rainbow-delimiters pcre2el org-plus-contrib org-bullets noflet flycheck evil-visualstar evil-surround evil-matchit evil-exchange evil-commentary diminish counsel company beacon aggressive-indent)))
+    (avy nlinum-relative linum-relative modern-cpp-font-lock ivy evil company base16-theme delight popup-kill-ring evil-numbers evil-args evil-string-inflection yasnippet which-key use-package string-inflection solarized-theme rainbow-mode rainbow-delimiters pcre2el org-plus-contrib org-bullets noflet flycheck evil-visualstar evil-surround evil-matchit evil-exchange evil-commentary diminish counsel beacon aggressive-indent)))
  '(safe-local-variable-values (quote ((org-confirm-babel-evaluate)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
