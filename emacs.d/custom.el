341ad88c0b391393546749fc047d610b8aa61ad9
Kartik Shenoy <theresmorethanoneway@gmail.com>
2018-03-30 07:34:38 -0600

    Added hydra, ivy-hydra

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -4,10 +4,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(debug-on-error t)
- '(nlinum-relative-redisplay-delay 0)
  '(package-selected-packages
    (quote
-    (avy nlinum-relative linum-relative modern-cpp-font-lock ivy evil company base16-theme delight popup-kill-ring evil-numbers evil-args evil-string-inflection yasnippet which-key use-package string-inflection solarized-theme rainbow-mode rainbow-delimiters pcre2el org-plus-contrib org-bullets noflet flycheck evil-visualstar evil-surround evil-matchit evil-exchange evil-commentary diminish counsel beacon aggressive-indent)))
+    (ivy-hydra hydra ivy-occur avy nlinum-relative linum-relative modern-cpp-font-lock ivy evil company base16-theme delight popup-kill-ring evil-numbers evil-args evil-string-inflection yasnippet which-key use-package string-inflection solarized-theme rainbow-mode rainbow-delimiters pcre2el org-plus-contrib org-bullets noflet flycheck evil-visualstar evil-surround evil-matchit evil-exchange evil-commentary diminish counsel beacon aggressive-indent)))
  '(safe-local-variable-values (quote ((org-confirm-babel-evaluate)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
