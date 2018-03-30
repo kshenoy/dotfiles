935652fa7ac47b126e284b62aba70e4b19ce8fa2
Kartik Shenoy <theresmorethanoneway@gmail.com>
2018-03-30 07:34:06 -0600

    Updates

--- emacs.d/emacs_minimal.el
+++ emacs.d/emacs_minimal.el
@@ -15,15 +15,12 @@
 (setq use-package-always-ensure t)
 
 ;; Install all packages
-(use-package solarized-theme
-  :config (load-theme 'solarized-light t))
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (solarized-theme use-package))))
+ '(package-selected-packages (quote (use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
