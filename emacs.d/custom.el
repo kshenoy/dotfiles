d7046ef24d8c23f9e5506e82320a828ba89c3d46
Kartik Shenoy <theresmorethanoneway@gmail.com>
2018-04-10 21:55:11 -0600

    Added magit

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -6,7 +6,7 @@
  '(debug-on-error t)
  '(package-selected-packages
    (quote
-    (ivy-hydra hydra ivy-occur avy nlinum-relative linum-relative modern-cpp-font-lock ivy evil company base16-theme delight popup-kill-ring evil-numbers evil-args evil-string-inflection yasnippet which-key use-package string-inflection solarized-theme rainbow-mode rainbow-delimiters pcre2el org-plus-contrib org-bullets noflet flycheck evil-visualstar evil-surround evil-matchit evil-exchange evil-commentary diminish counsel beacon aggressive-indent)))
+    (magit ivy-hydra hydra ivy-occur avy nlinum-relative linum-relative modern-cpp-font-lock ivy evil company base16-theme delight popup-kill-ring evil-numbers evil-args evil-string-inflection yasnippet which-key use-package string-inflection solarized-theme rainbow-mode rainbow-delimiters pcre2el org-plus-contrib org-bullets noflet flycheck evil-visualstar evil-surround evil-matchit evil-exchange evil-commentary diminish counsel beacon aggressive-indent)))
  '(safe-local-variable-values (quote ((org-confirm-babel-evaluate)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
