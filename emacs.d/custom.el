e0150177c28a7fe34bcb36aea1299be72a4ceae3
Kartik Shenoy <theresmorethanoneway@gmail.com>
2018-06-28 14:07:40 -0600

    Added org-expiry

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -7,7 +7,7 @@
  '(debug-on-error t)
  '(package-selected-packages
    (quote
-    (badger-theme magit ivy-hydra hydra ivy-occur avy nlinum-relative linum-relative modern-cpp-font-lock ivy evil company delight popup-kill-ring evil-numbers evil-args evil-string-inflection yasnippet which-key use-package string-inflection solarized-theme rainbow-mode rainbow-delimiters pcre2el org-plus-contrib org-bullets noflet flycheck evil-visualstar evil-surround evil-matchit evil-exchange evil-commentary diminish counsel beacon aggressive-indent)))
+    (org-expiry magit ivy-hydra hydra ivy-occur avy nlinum-relative linum-relative modern-cpp-font-lock ivy evil company delight popup-kill-ring evil-numbers evil-args evil-string-inflection yasnippet which-key use-package string-inflection solarized-theme rainbow-mode rainbow-delimiters pcre2el org-plus-contrib org-bullets noflet flycheck evil-visualstar evil-surround evil-matchit evil-exchange evil-commentary diminish counsel beacon aggressive-indent)))
  '(safe-local-variable-values (quote ((org-confirm-babel-evaluate))))
  '(show-paren-mode t)
  '(tool-bar-mode nil))
