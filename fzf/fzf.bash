b8c3b61b735370f1a0df8f6c9c45dc42add2af38
Kartik Shenoy <theresmorethanoneway@gmail.com>
2018-11-22 19:18:59 -0700

    Updates

--- fzf/fzf.bash
+++ fzf/fzf.bash
@@ -1,3 +1,14 @@
+if [[ -z "$FZF_PATH" ]]; then
+  _fg_red=$(tput setaf 1)
+  _reset=$(tput sgr0)
+  echo "${_fg_red}ERROR${_reset}: FZF_PATH is not set"
+  return
+fi
+
+# Setup fzf
+# ---------
+[[ ! "$PATH" == *$FZF_PATH/bin* ]] && export PATH="$PATH:$FZF_PATH/bin"
+
 # Auto-completion
 # ---------------
 [[ $- == *i* ]] && source "$FZF_PATH/shell/completion.bash" 2> /dev/null
@@ -5,9 +16,9 @@
 # Key bindings
 # ------------
 # Note the order is important because some functions get overridden
-. $FZF_PATH/shell/key-bindings.bash
-. $HOME/.dotfiles/fzf/fzf_vcs.bash
-. $HOME/.dotfiles/fzf/key-bindings.bash
+source $FZF_PATH/shell/key-bindings.bash
+source $HOME/.dotfiles/fzf/fzf_vcs.bash
+source $HOME/.dotfiles/fzf/key-bindings.bash
 
 # Customisations
 # --------------
