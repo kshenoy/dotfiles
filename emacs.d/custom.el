e6f7c7305395314d7b1338fd1e806f8eceefec76
Kartik Shenoy <theresmorethanoneway@gmail.com>
2019-01-05 13:38:18 -0700

    Updates

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -3,14 +3,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(evil-emacs-state-cursor (quote ("#839496" bar)) t)
- '(evil-insert-state-cursor (quote ("#268bd2" bar)) t)
- '(evil-normal-state-cursor (quote ("#859900" box)) t)
- '(evil-operator-state-cursor (quote ("#dc322f" hollow)) t)
- '(evil-replace-state-cursor (quote ("#dc322f" bar)) t)
- '(evil-visual-state-cursor (quote ("#b58900" box)) t)
- '(package-selected-packages (quote (org-plus-contrib use-package)))
- '(safe-local-variable-values (quote ((org-confirm-babel-evaluate)))))
+ '(evil-emacs-state-cursor '("#839496" bar) t)
+ '(evil-insert-state-cursor '("#268bd2" bar) t)
+ '(evil-normal-state-cursor '("#859900" box) t)
+ '(evil-operator-state-cursor '("#dc322f" hollow) t)
+ '(evil-replace-state-cursor '("#dc322f" bar) t)
+ '(evil-visual-state-cursor '("#b58900" box) t)
+ '(package-selected-packages '(org-plus-contrib use-package))
+ '(safe-local-variable-values '((org-confirm-babel-evaluate))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
