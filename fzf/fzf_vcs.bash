4fdfb8ee4ca2a9250bec62cbe240ad354d1e7fd5
Kartik Shenoy <theresmorethanoneway@gmail.com>
2019-08-08 14:20:00 -0600

    Moved all vcs functions to a separate file and updated prompt to display p4 branch info

--- fzf/fzf_vcs.bash
+++ fzf/fzf_vcs.bash
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
-is_in_git_repo() {                                                                                                 #{{{1
-  git rev-parse HEAD &> /dev/null
-}
-
-is_in_perforce_repo() {                                                                                            #{{{1
-  p4 info &> /dev/null
-}
-
 __fzf-p4-strip-common-ancestors() {                                                                                #{{{1
   for item in "$@"; do
     [[ -z "$item" ]] && continue
@@ -29,7 +21,7 @@ __fzf-down() {                                                                  
 
 
 fzf-git-diffs() {                                                                                                  #{{{1
-  is_in_git_repo || return
+  vcs__is_in_git_repo || return
   git -c color.status=always status --short |
   fzf -m --ansi --nth 2..,.. \
     --preview '(git diff --color=always -- {-1} | sed 1,4d; cat {-1}) | head -500' |
@@ -38,7 +30,7 @@ fzf-git-diffs() {                                                               
 
 
 fzf-git-branches() {                                                                                               #{{{1
-  is_in_git_repo || return
+  vcs__is_in_git_repo || return
   git branch -a --color=always | grep -v '/HEAD\s' | sort |
   __fzf-down --ansi --multi --tac --preview-window right:70% \
     --preview 'git log --oneline --graph --date=short --pretty="format:%C(auto)%cd %h%d %s" $(sed s/^..// <<< {} | cut -d" " -f1) | head -'$LINES |
@@ -48,7 +40,7 @@ fzf-git-branches() {                                                            
 
 
 fzf-git-tags() {                                                                                                   #{{{1
-  is_in_git_repo || return
+  vcs__is_in_git_repo || return
   git tag --sort -version:refname |
   __fzf-down --multi --preview-window right:70% \
     --preview 'git show --color=always {} | head -'$LINES
@@ -56,7 +48,7 @@ fzf-git-tags() {                                                                
 
 
 fzf-git-hashes() {                                                                                                 #{{{1
-  is_in_git_repo || return
+  vcs__is_in_git_repo || return
   git log --date=short --format="%C(green)%C(bold)%cd %C(auto)%h%d %s (%an)" --graph --color=always |
   __fzf-down --ansi --no-sort --reverse --multi --bind 'ctrl-s:toggle-sort' \
     --header 'Press CTRL-S to toggle sort' \
@@ -66,7 +58,7 @@ fzf-git-hashes() {                                                              
 
 
 fzf-git-remotes() {                                                                                                #{{{1
-  is_in_git_repo || return
+  vcs__is_in_git_repo || return
   git remote -v | awk '{print $1 "\t" $2}' | uniq |
   __fzf-down --tac \
     --preview 'git log --oneline --graph --date=short --pretty="format:%C(auto)%cd %h%d %s" {1} | head -200' |
@@ -75,7 +67,7 @@ fzf-git-remotes() {                                                             
 
 
 __fzf_p4_walist__() {                                                                                              #{{{1
-  FZF_ALT_C_COMMAND='wa_list' __fzf_cd__
+  FZF_ALT_C_COMMAND='wals' __fzf_cd__
 }
 
 
@@ -95,7 +87,7 @@ __fzf-p4-cd() {                                                                 
 
 
 fzf-vcs-cd() {                                                                                                     #{{{1
-  if is_in_perforce_repo; then
+  if vcs__is_in_perforce_repo; then
     __fzf-p4-cd
   else
     __fzf_cd__
@@ -104,40 +96,51 @@ fzf-vcs-cd() {                                                                  
 
 
 fzf-vcs-all-files() {                                                                                              #{{{1
-  if is_in_git_repo; then
-    FZF_CTRL_T_COMMAND='{ git ls-tree --full-tree -r --name-only HEAD || \
-      find . -path "*/\.*" -prune -o -type f -print -o -type l -print | sed s/^..//; } 2> /dev/null' \
-      fzf-file-widget
-  elif is_in_perforce_repo; then
-    FZF_CTRL_T_COMMAND='cat $STEM/.filelist \
-                            <(command p4 opened 2> /dev/null | command grep add | command sed "s/#.*//" | \
-                              command xargs -I{} -n1 command p4 where {} | command awk "{print \$3}") \
-                            $STEM/build/latest/generated/.filelist 2> /dev/null' fzf-file-widget
+  if vcs__is_in_git_repo; then
+    local _top=$(git rev-parse --show-toplevel)
+    local _selected=$(git ls-tree --full-tree -r --name-only HEAD |
+      FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} --reverse $FZF_DEFAULT_OPTS $FZF_CTRL_T_OPTS" fzf -m |
+      while read -r item; do
+        path=${_top}/$item
+        printf '%q ' "${path#$(realpath $PWD)/}"
+      done
+    )
+
+    READLINE_LINE="${READLINE_LINE:0:$READLINE_POINT}$_selected${READLINE_LINE:$READLINE_POINT}"
+    READLINE_POINT=$(( READLINE_POINT + ${#_selected} ))
+  elif vcs__is_in_perforce_repo; then
+    FZF_CTRL_T_COMMAND='cat \
+        <(command p4 have $STEM/...) \
+        <(command p4 opened | command grep add | command sed "s/#.*//" |
+          command xargs -I{} -n1 command p4 where {}) 2> /dev/null |
+      command awk "{print \$3}" | command sort -u' \
+    fzf-file-widget
   else
     fzf-file-widget
   fi
-  echo
 }
 
 
 fzf-vcs-files() {                                                                                                  #{{{1
-  if is_in_git_repo; then
+  if vcs__is_in_git_repo; then
     FZF_CTRL_T_COMMAND='{ git ls-tree -r --name-only HEAD || \
       find . -path "*/\.*" -prune -o -type f -print -o -type l -print | sed s/^..//; } 2> /dev/null' \
       fzf-file-widget
-  elif is_in_perforce_repo; then
-    FZF_CTRL_T_COMMAND='{ {p4 have ... | command awk "{print \$3}" | command sed "s:$STEM/::"} || \
-      find . -path "*/\.*" -prune -o -type f -print -o -type l -print | sed s/^..//; } 2> /dev/null' \
-      fzf-file-widget
+  elif vcs__is_in_perforce_repo; then
+    FZF_CTRL_T_COMMAND='cat \
+        <(p4 have ... | command awk "{print \$3}") \
+        <(command p4 opened 2> /dev/null | command grep add | command sed "s/#.*//" | \
+          command xargs -I{} -n1 command p4 where {} | command awk "{print \$3}") 2> /dev/null | \
+      command sed "s:$PWD/::"' \
+    fzf-file-widget
   else
     fzf-file-widget
   fi
-  echo
 }
 
 
 fzf-vcs-status() {                                                                                                 #{{{1
-  if is_in_git_repo; then
+  if vcs__is_in_git_repo; then
     local _selected=$(git -c color.status=always status --short |
       fzf -m --nth=2 |
       awk '{print $NF}' |
@@ -145,11 +148,12 @@ fzf-vcs-status() {                                                              
         printf '%q ' "$item"
       done
     )
-  elif is_in_perforce_repo; then
+  elif vcs__is_in_perforce_repo; then
     local _selected=$(p4 opened 2> /dev/null | sed -r -e "s:^//depot/[^/]*/(trunk|branches/[^/]*)/::" |
       column -s# -o "    #" -t | column -s- -o- -t | fzf -m --nth=1 | awk '{print $1}' |
       while read -r item; do
-        printf '%q ' "$STEM/$item"
+        path=${STEM}/$item
+        printf '%q ' "${path#$(realpath $PWD)/}"
       done
     )
   fi
