38a6dc70975574ee38490c139c21e5fd954967ec
Kartik Shenoy <theresmorethanoneway@gmail.com>
2020-10-31 11:31:22 -0600

    Emacs config clean-up
    * Fixed bug in tangling of emacs config which was replacing the init.el
      link with partially tangled file
    * Deleted my-org-flatten-link function since I don't really use it
    * Deleted org-drill. Anki is way more convenient to use
    * Removed org-tempo from org-modules

--- emacs/custom.el
+++ emacs/custom.el
@@ -14,7 +14,8 @@
     (use-package-hydra eyebrowse pos-tip popup s magit-popup git-commit with-editor async pkg-info epl string-inflection goto-chg org-protocol-capture-html quelpa-use-package quelpa company-tng try magit company-box delight yasnippet which-key rainbow-mode rainbow-delimiters popup-kill-ring pcre2el org-bullets noflet modern-cpp-font-lock ivy-rich counsel swiper ivy-hydra ivy hydra flycheck evil-visualstar evil-surround evil-string-inflection evil-numbers evil-matchit evil-exchange evil-commentary evil-args evil company-irony company beacon avy aggressive-indent org-plus-contrib use-package)))
  '(safe-local-variable-values
    (quote
-    ((eval add-hook
+    ((org-confirm-babel-evaluate)
+     (eval add-hook
            (quote after-save-hook)
            (lambda nil
              (org-babel-tangle))
