3121aaa3a767e7a5914eec9249c4749971c9bd4e
Kartik Shenoy <theresmorethanoneway@gmail.com>
2020-11-28 13:20:12 -0700

    Made faces prettier and easier to read

--- emacs/doom/custom.el
+++ emacs/doom/custom.el
@@ -3,7 +3,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(safe-local-variable-values (quote ((org-confirm-babel-evaluate)))))
+ '(safe-local-variable-values
+   (quote
+    ((org-refile-targets
+      (nil :maxlevel . 9))
+     (org-enforce-todo-dependencies)
+     (org-enforce-todo-checkbox-dependencies)
+     (org-confirm-babel-evaluate)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
