b2eaab6b3361bc4692b0247c7ec56070fb8e5d1c
Kartik Shenoy <theresmorethanoneway@gmail.com>
2020-12-15 10:25:16 -0700

    Updates

--- emacs/minimal/init.el
+++ emacs/minimal/init.el
@@ -15,15 +15,4 @@
 (setq use-package-always-ensure t)
 
 ;; Install all packages
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;; (use-package solarized-theme)
