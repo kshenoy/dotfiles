15931dac38e80d4cba60b8ba7bc0c4c06bf138b2
Kartik Shenoy <theresmorethanoneway@gmail.com>
2022-08-20 00:07:08 -0600

    Disabling most plugins to try to make emacs more responsive

--- emacs/doom/custom.el
+++ emacs/doom/custom.el
@@ -3,9 +3,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   '("~/Documents/Notes/Work/Monarch.org" "/home/kshenoy/Documents/Notes/Work/AMD.org" "/home/kshenoy/Documents/Notes/Work/Eldora.org"))
  '(safe-local-variable-values
-   (quote
-    ((org-confirm-babel-evaluate)))))
+   '((git-commit-major-mode . git-commit-elisp-text-mode)
+     (org-confirm-babel-evaluate))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- emacs/doom/init.el
+++ emacs/doom/init.el
@@ -20,8 +20,8 @@
        ;; layout                          ; auie,ctsrnm is the superior home row
 
        :completion
-       (company                        ; the ultimate code completion backend
-        +childframe)                   ; ... when your children are better than you
+       ;; (company                        ; the ultimate code completion backend
+       ;;  +childframe)                   ; ... when your children are better than you
        ;; helm                            ; the *other* search engine for love and life
        ;; ido                             ; the other *other* search engine...
        (ivy +icons)                    ;  a search engine for love and life
@@ -31,21 +31,21 @@
        :ui
        ;; deft                            ; notational velocity for Emacs
        doom                            ;  what makes DOOM look the way it does
-       doom-dashboard                  ;  a nifty splash screen for Emacs
-       doom-quit                       ;  DOOM quit-message prompts when you quit Emacs
+       ;; doom-dashboard                  ;  a nifty splash screen for Emacs
+       ;; doom-quit                       ;  DOOM quit-message prompts when you quit Emacs
        ;; (emoji +unicode)                ; 🙂
        ;; hl-todo                         ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;; hydra
        ;; indent-guides                   ;  highlighted indent columns
        (ligatures +extra)     ; ligatures and symbols to make your code pretty again
        ;; minimap                         ; show a map of the code on the side
-       modeline                        ;  snazzy, Atom-inspired modeline, plus API
+       (modeline +light)               ;  snazzy, Atom-inspired modeline, plus API
        nav-flash                       ; blink cursor line after big motions
        ;; neotree                         ; a project drawer, like NERDTree for vim
        ophints                         ;  highlight the region an operation acts on
-       (popup                          ; tame sudden yet inevitable temporary windows
-        +all                           ; ... catch all popups that start with an asterix
-        +defaults)                     ; ... default popup rules
+       ;; (popup                          ; tame sudden yet inevitable temporary windows
+       ;;  +all                           ; ... catch all popups that start with an asterix
+       ;;  +defaults)                     ; ... default popup rules
        ;; tabs                            ; a tab bar for Emacs
        ;; treemacs                        ; a project drawer, like neotree but cooler
        ;; unicode                         ; extended unicode support for various languages
@@ -58,7 +58,7 @@
 
        :editor
        (evil +everywhere)              ;  come to the dark side, we have cookies
-       file-templates                  ;  auto-snippets for empty files
+       ;; file-templates                  ;  auto-snippets for empty files
        ;; fold                            ; (nigh) universal code folding
        ;; (format +onsave)                ; automated prettiness
        ;; god                             ; run Emacs commands without modifier keys
@@ -67,7 +67,7 @@
        ;; objed                           ; text object editing for the innocent
        ;; (parinfer +rust)                ;  turn lisp into python, sort of
        ;; rotate-text                     ; cycle region at point between text candidates
-       snippets                        ;  my elves. They type so I don't have to
+       ;; snippets                        ;  my elves. They type so I don't have to
        word-wrap                       ;  soft wrapping with language-aware indent
 
        :emacs
@@ -146,7 +146,7 @@
        ;; lean
        ;; factor
        ;; ledger                          ; an accounting system in Emacs
-       ;; lua                             ; one-based indices? one-based indices
+       lua                             ; one-based indices? one-based indices
        ;; markdown                        ; writing docs for people to ignore
        ;; nim                             ; python + lisp at the speed of c
        ;; nix                             ; I hereby declare "nix geht mehr!"
