aa2f1116ad416bec4bf3c7abc1f1be1fbeb974bb
Kartik Shenoy <theresmorethanoneway@gmail.com>
2022-08-20 02:01:23 -0600

    Removed old chemacs profiles

--- chemacs/profiles.el
+++ chemacs/profiles.el
@@ -1,4 +1,2 @@
 (("default" . ((user-emacs-directory . "~/.config/emacs-doom")))
- ("doom"    . ((user-emacs-directory . "~/.config/emacs-doom")))
- ("minimal" . ((user-emacs-directory . "~/.config/dotfiles/emacs/minimal")))
- ("vanilla" . ((user-emacs-directory . "~/.config/dotfiles/emacs/vanilla"))))
+ ("doom"    . ((user-emacs-directory . "~/.config/emacs-doom"))))
