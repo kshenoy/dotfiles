ccd263704365dad2be1fca09987ed78bff176efd
Kartik Shenoy <theresmorethanoneway@gmail.com>
2022-12-13 07:01:47 -0700

    Added catppuccin themes for FZF

---
+++ fzf/themes/catppuccin-latte.sh
@@ -0,0 +1,4 @@
+export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
+--color=bg+:#ccd0da,bg:#eff1f5,spinner:#dc8a78,hl:#d20f39 \
+--color=fg:#4c4f69,header:#d20f39,info:#8839ef,pointer:#dc8a78 \
+--color=marker:#dc8a78,fg+:#4c4f69,prompt:#8839ef,hl+:#d20f39"
