e8bbece12b1a09476e6499b8d776b54d3d8dd3ec
Kartik Shenoy <theresmorethanoneway@gmail.com>
2022-12-13 06:28:20 -0700

    Updated alias to use bat if available

--- fish/functions/C.fish
+++ fish/functions/C.fish
@@ -1,5 +1,9 @@
 # [[file:~/.config/dotfiles/fish/fish.org::*aliases][aliases:10]]
 function C --wraps=cat
-  cat $argv
+  if type -q bat
+    bat $argv
+  else
+    cat $argv
+  end
 end
 # aliases:10 ends here
