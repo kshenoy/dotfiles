1c760ac6cfbd63ed0ae9ef4281cd4b226811d06a
Kartik Shenoy <theresmorethanoneway@gmail.com>
2023-10-27 21:09:54 -0700

    Updates

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,13 @@
-# Use [[https://starship.rs/][starship prompt]]
-starship init fish | source
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
 
 fish_add_path ~/.local/bin
 fish_add_path ~/.cargo/bin
-# fish_config theme save "Catppuccin Macchiato"
 
-zoxide init --cmd j fish | source
+# fish_config theme save "Catppuccin Frappe"
+
+# Use starship prompt (https://starship.rs/)
+starship init fish | source
+
+# zoxide init --cmd j fish | source
