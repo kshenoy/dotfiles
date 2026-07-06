748fbeb38ce37c6796ac8ab9ffc4e2ee0a5e1193
Kartik Shenoy <theresmorethanoneway@gmail.com>
2026-07-06 16:44:33 -0700

    Made clod a generic function

--- fish/conf.d/hosts/Kartiks-MacBook-Pro.local.fish
+++ fish/conf.d/hosts/Kartiks-MacBook-Pro.local.fish
@@ -7,3 +7,7 @@ end
 eval (/opt/homebrew/bin/brew shellenv)
 
 set -gx FZF_GIT_HOME /opt/fzf-git.sh
+
+function gitn --wraps git --description 'git for the Coppermind notes repo'
+    git -C ~/Documents/Notes/Coppermind $argv
+end
--- fish/conf.d/hosts/services.local.fish
+++ fish/conf.d/hosts/services.local.fish
@@ -1,12 +1,8 @@
 # All services VM specific configuration goes here
-if test (hostname) != "services"
+if test (hostname) != services
     return
 end
 
 function gitn --wraps git --description 'git for the Coppermind notes repo'
-    git -C ~/Documents/Notes/Coppermind $argv
-end
-
-function clod --wraps claude --description 'Accelerate claude startup'
-    claude --remote-control "$argv[1]" --name "$argv[1]"
+    git -C ~/Coppermind $argv
 end
